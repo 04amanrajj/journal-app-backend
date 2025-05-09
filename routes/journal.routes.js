19b30b3b20160fe1fb5491373a771360e91c7e14
04amanrajj <04.aman.raj@gmail.com>
2025-05-09 16:35:55 +0530

    feat: implement file upload route with ZIP file validation

--- routes/journal.routes.js
+++ routes/journal.routes.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const multer = require("multer");
+
+
 const {
   createJournal,
   getJournals,
   editJournal,
   deleteJournal,
+  uploadJournal
 } = require("../controllers/journal.controller");
 
 const journalRoutes = express.Router();
@@ -19,5 +23,28 @@ journalRoutes.put("/edit/:id", editJournal);
 
 // Route to delete a journal
 journalRoutes.delete("/delete/:id", deleteJournal);
+
+// Route to upload a file
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+const upload = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype === 'application/zip') {
+      cb(null, true);
+    } else {
+      cb(new Error('Only ZIP files are allowed'), false);
+    }
+  }
+});
+
+journalRoutes.post("/upload", upload.single("file"), uploadJournal)
 
 module.exports = { journalRoutes };
