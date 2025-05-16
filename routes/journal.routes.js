d68ac0e3a97cf6d80690fc3e76888e5cc7506f65
04amanrajj <04.aman.raj@gmail.com>
2025-05-16 23:59:48 +0530

    feat: add S3 upload route and refactor file upload handling

--- routes/journal.routes.js
+++ routes/journal.routes.js
@@ -1,13 +1,13 @@
 const express = require("express");
-const multer = require("multer");
-
+const upload = require("../middlewares/upload.middleware");
 
 const {
   createJournal,
   getJournals,
   editJournal,
   deleteJournal,
-  uploadJournal
+  uploadJournal,
+  uploadJournalS3
 } = require("../controllers/journal.controller");
 
 const journalRoutes = express.Router();
@@ -25,26 +25,11 @@ journalRoutes.put("/edit/:id", editJournal);
 journalRoutes.delete("/delete/:id", deleteJournal);
 
 // Route to upload a file
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
-  },
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname}`);
-  },
-});
-
-const upload = multer({
-  storage,
-  fileFilter: (req, file, cb) => {
-    if (file.mimetype === 'application/zip') {
-      cb(null, true);
-    } else {
-      cb(new Error('Only ZIP files are allowed'), false);
-    }
-  }
-});
-
 journalRoutes.post("/upload", upload.single("file"), uploadJournal)
+
+// Route to upload a file to S3
+journalRoutes.post("/upload-s3", upload.single("file"), uploadJournalS3)
+
+
 
 module.exports = { journalRoutes };
