67f076b8f11e807099621822a1f13469b2c4673a
04amanrajj <04.aman.raj@gmail.com>
2025-05-16 23:59:27 +0530

    feat: add MinIO S3 client configuration for local storage

---
+++ config/minioClient.js
@@ -0,0 +1,14 @@
+const { S3Client } = require("@aws-sdk/client-s3");
+
+const s3 = new S3Client({
+    endpoint: "http://localhost:9000",
+    region: "us-east-1",
+    credentials: {
+        accessKeyId: "minioadmin",
+        secretAccessKey: "minioadmin",
+    },
+    forcePathStyle: true,
+    secretAccessKey: "minioadmin",
+});
+
+module.exports = s3;
