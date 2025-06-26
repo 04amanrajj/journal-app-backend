daf1cba4bd8070986eee3edb40280f81e1bc6c21
04amanrajj <04.aman.raj@gmail.com>
2025-06-26 19:50:28 +0530

    refactor: remove comments

--- middlewares/upload.middleware.js
+++ middlewares/upload.middleware.js
@@ -6,7 +6,7 @@ const upload = multer({ storage });
 module.exports = upload;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME, // Click 'View API Keys' above to copy your cloud name
-  api_key: process.env.CLOUDINARY_KEY, // Click 'View API Keys' above to copy your API key
-  api_secret: process.env.CLOUDINARY_SECRET, // Click 'View API Keys' above to copy your API secret
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_KEY,
+  api_secret: process.env.CLOUDINARY_SECRET,
 });
