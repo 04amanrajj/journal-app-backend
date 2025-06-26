43b305382138438cbc2103d8f846384d0140ed5a
04amanrajj <04.aman.raj@gmail.com>
2025-06-26 19:49:41 +0530

    update: add cloudinery configuration

--- middlewares/upload.middleware.js
+++ middlewares/upload.middleware.js
@@ -1,6 +1,12 @@
 const multer = require("multer");
-
+const cloudinary = require("cloudinary").v2;
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
 module.exports = upload;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME, // Click 'View API Keys' above to copy your cloud name
+  api_key: process.env.CLOUDINARY_KEY, // Click 'View API Keys' above to copy your API key
+  api_secret: process.env.CLOUDINARY_SECRET, // Click 'View API Keys' above to copy your API secret
+});
